4886d35f863fc4efa0bbdad26520643613f85e89
Mariano97m <marianomartinez632@gmail.com>
2024-06-12 16:40:28 -0400

    Primer React con JSX

---
+++ src/componente/Lista/Lista.jsx
@@ -0,0 +1,13 @@
+
+
+const Lista = () => {
+    return (
+        <ul>
+            <li>Lean Reacy</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    );
+}
+export default Lista;
